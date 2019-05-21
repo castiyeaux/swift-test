fdeaf14787d1ded432f17d0c2040ad00ff9c3588
Andrea Castillo <andreacastillo@me.com>
2019-05-21 00:09:13 -0500

    Add MusicPlayer app

---
+++ MusicPlayer/MusicPlayer/GenreButtonScreen.swift
@@ -0,0 +1,30 @@
+//
+//  GenreButtonScreen.swift
+//  MusicPlayer
+//
+//  Created by Andrea Castillo on .
+//  Copyright © 2019 Andrea Castillo. All rights reserved.
+//
+
+import UIKit
+
+class GenreButtonScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
